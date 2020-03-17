8f8769e63781c398cd2759e46492ebc959f65830
Janis <feldmanis45@gmail.com>
2020-03-17 15:13:35 +0200

    added twitter

---
+++ ui/src/Components/Twitter/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+    TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton,
+    TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare,
+    TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton
+} from 'react-twitter-embed';
+
+const twiter = () => {
+
+
+
+    return (
+        <>
+            <TwitterTimelineEmbed
+                sourceType="SPKCentrs"
+                screenName="SPKCentrs"
+                options={{ height: 400 ,
+                    width:200}}
+            />
+
+        </>
+
+    );
+
+
+};
+
+export default twiter;
