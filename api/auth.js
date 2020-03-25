e74369bc57579448f42a60e76152987712bb9cce
snotrman <nils.riga@gmail.com>
2020-03-25 13:30:16 +0200

    still fixing cors

--- api/auth.js
+++ api/auth.js
@@ -3,7 +3,10 @@ import jwt from 'jsonwebtoken';
 export default async(req, res, next) => {
 
     try {
-        if (jwt.verify(req.headers.authorization, process.env.JWT_KEY).secret === process.env.JWT_SECRET && (req.headers.origin === process.env.UI_URL || req.headers.origin === process.env.UI_URL_LV)) {
+        if (
+            jwt.verify(req.headers.authorization, process.env.JWT_KEY).secret === process.env.JWT_SECRET &&
+            (req.headers.origin === process.env.UI_URL || req.headers.origin === process.env.UI_URL_LV || req.headers.origin === process.env.UI_URL_WWW || req.headers.origin === process.env.UI_URL_LV_WWW)
+        ) {
             next()
         } else {
             res.writeHead(301, { Location: process.env.UI_URL })
