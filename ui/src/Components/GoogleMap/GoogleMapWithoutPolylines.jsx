b6b3799064e79303f8c7278197ff8c63d785ee43
snotrman <nils.riga@gmail.com>
2020-04-06 15:52:46 +0300

    cleaned up a bit

--- ui/src/Components/GoogleMap/GoogleMapWithoutPolylines.jsx
+++ ui/src/Components/GoogleMap/GoogleMapWithoutPolylines.jsx
@@ -6,35 +6,21 @@ import {
   GoogleMap,
   Marker,
   InfoWindow,
-  // Polyline,
-  // Circle
-  // DirectionsRenderer
 } from "react-google-maps"
 import MyStyle from "./MyStyleJSON.js"
 import moment from "moment"
 import biohazardIcon from "./bioHazzardIcon.png"
 import "moment/locale/lv"
-// import jwt from "jsonwebtoken"
 const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
 moment.locale('lv')
 
 
 let state = {
-  // data: [],
-  // lineSymbol: {},
   markerIcon: {},
   labelOrigin: {},
   i: 0,
   total: 0,
   timeNow: moment().format('DD.MMM.YYYY'),
-  // auth: {
-  //   method: "GET",
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Accept': 'application/json',
-  //     'Authorization': jwt.sign({ secret: process.env.REACT_APP_JWT_SECRET }, process.env.REACT_APP_JWT_KEY)
-  //   }
-  // }
 }
 
 
@@ -91,65 +77,9 @@ const MapWithoutPolylines = compose(
   }),
   lifecycle({
     componentWillMount() {
-      // this.setState({ data: [] });
+
     },
     componentDidMount() {
-      console.log("map wihtout polilines")
-      // THIS ENABLES DIRECTIONS
-      // const DirectionsService = new window.google.maps.DirectionsService();
-      // linePath.strokeOpacity = 1.0
-      // linePath.scale = 4
-
-
-      // state.labelOrigin = new window.google.maps.Point(12, 66)
-
-      // state.lineSymbol = {
-      //   path: window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW
-      // }
-
-      // fetch(process.env.REACT_APP_API_HOST, state.auth)
-      //   .then(res => res.json())
-      //   .then(
-      //     (result) => {
-      //       state.data = result
-      //       state.total = result.length
-      //     },
-      //     (error) => {
-      //       console.log(error)
-      //       state.data = error
-      //     }
-      //   )
-      //   .then(data => this.setState({ data }))
-      //   .then(data => {
-      //     // THIS ENABLES DIRECTIONS
-      //     // state.data.forEach(item => {
-      //     //   const selfLat = parseFloat(item.selfCooLat)
-      //     //   const selfLng = parseFloat(item.selfCooLng)
-      //     //   const originLat = parseFloat(item.originCooLat)
-      //     //   const originLng = parseFloat(item.originCooLng)
-      //     //   DirectionsService.route({
-      //     //     origin: {
-      //     //       lat: originLat,
-      //     //       lng: originLng
-      //     //     },
-      //     //     destination: {
-      //     //       lat: selfLat,
-      //     //       lng: selfLng
-      //     //     },
-      //     //     travelMode: window.google.maps.TravelMode.WALKING,
-      //     //   }, (result, status) => {
-      //     //     if (status === window.google.maps.DirectionsStatus.OK) {
-      //     //       this.setState({
-      //     //         directions: result,
-      //     //       });
-      //     //     } else {
-      //     //       console.error(`error fetching directions ${result}`);
-      //     //     }
-      //     //   });
-      //     // })
-
-      //   })
-
 
     }
   })
@@ -169,90 +99,28 @@ const MapWithoutPolylines = compose(
       enableRetinaIcons
       gridSize={11}
       ignoreHidden={true}
-    // ENABLE THIS FOR CUSTOM CLUSTER ICON
-    // styles={[
-    //   {
-    //     textColor: '#c2fff1',
-    //     height: 100,
-    //     url: "https://iconsplace.com/wp-content/uploads/_icons/fffff0/32/png/biohazard-icon-7-32.png",
-    //     width: 100,
-    //   },
-    //   {
-    //     textColor: '#c2fff1',
-    //     height: 56,
-    //     url: "https://iconsplace.com/wp-content/uploads/_icons/fffff0/32/png/biohazard-icon-7-32.png",
-    //     width: 56,
-    //   },
-    //   {
-    //     textColor: '#c2fff1',
-    //     height: 66,
-    //     url: "https://iconsplace.com/wp-content/uploads/_icons/fffff0/32/png/biohazard-icon-7-32.png",
-    //     width: 66,
-    //   },
-    //   {
-    //     textColor: '#c2fff1',
-    //     height: 78,
-    //     url: "https://iconsplace.com/wp-content/uploads/_icons/fffff0/32/png/biohazard-icon-7-32.png",
-    //     width: 78,
-    //   },
-    //   {
-    //     textColor: '#c2fff1',
-    //     height: 90,
-    //     url: "https://iconsplace.com/wp-content/uploads/_icons/fffff0/32/png/biohazard-icon-7-32.png",
-    //     width: 90,
-    //   }]}
     >
 
 
 
-      {/* {THIS ENABLES DIRECTIONS} */}
-      {/* {props.directions && <DirectionsRenderer directions={props.directions} />} */}
       {
         props.infectedPeople.map((item, index, arr) => {
 
 
-          // const selfLat = parseFloat(item.selfCooLat)
-          // const selfLng = parseFloat(item.selfCooLng)
-          // const originLat = parseFloat(item.originCooLat)
-          // const originLng = parseFloat(item.originCooLng)
-
           // THIS MAKES MARKERS BE SPREAD OUT A MORE
           const selfLat = parseFloat(getOffsetCoo(item.selfCooLat))
           const selfLng = parseFloat(getOffsetCoo(item.selfCooLng))
-          // const originLat = parseFloat(getOffsetCoo(item.originCooLat))
-          // const originLng = parseFloat(getOffsetCoo(item.originCooLng))
 
 
 
           state.markerIcon = {
             url: biohazardIcon,
-            // scaledSize: new window.google.maps.Size(25, 25),
             origin: new window.google.maps.Point(0, 0),
-            // anchor: new window.google.maps.Point(getOffsetXY(30), getOffsetXY(60)),
             labelOrigin: new window.google.maps.Point(getOffsetXY(35), getOffsetXY(65))
           }
 
           return (
             <div key={Math.random() * 10000000000000000}>
-
-
-              {/* < Marker
-                key={index + "origin"}
-                position={{
-                  lat: originLat,
-                  lng: originLng
-                }}
-                visible={false}
-                // ENABLE THIS FOR CUSTOM MARKER ICON
-                // icon={state.markerIcon}
-                label={{
-                  text: item.label,
-                  color: "#FFFFFF",
-                  fontSize: "16px",
-                  labelOrigin: state.labelOrigin
-                }}
-              /> */}
-
 
 
 
@@ -292,99 +160,10 @@ const MapWithoutPolylines = compose(
                   </div>
                 )}
                 label={item.id === "1" ? item.label : ""}
-                // openedInfoWindowId={{ windowId: props.openedInfoWindowId, selfId: item.id }}
+              // openedInfoWindowId={{ windowId: props.openedInfoWindowId, selfId: item.id }}
               />
 
 
-              {/* <Marker
-                key={index + "Coordinates"}
-                position={{
-                  lat: selfLat,
-                  lng: selfLng
-                }}
-                icon={state.markerIcon}
-                onClick={props.onToggleOpen}
-              >
-
-                {
-                props.isOpen && <InfoWindow
-                  key={index + "Info"}
-                  onCloseClick={props.onToggleOpen}
-                >
-                  <div style={{ opacity: 0.75 }}>
-                    <div>
-                      <ul>
-
-
-                        {item.label ? <h3>{item.label}</h3> : ""}
-                        {item.origin ? <li>Izcelsme: {item.origin}</li> : ""}
-                        {item.totalInfected ? <li>Inficētais Nr.: {item.totalInfected}</li> : ""}
-                        {item.dateOfFirstContactWIthLatvia ? <li>Pirmais Kontakts ar Latviju: {item.dateOfFirstContactWIthLatvia}</li> : ""}
-                        {item.dateOfDiagnosisBroadcast ? <li>Izsludināšanas Datums: {item.dateOfDiagnosisBroadcast}</li> : ""}
-                        {item.descriptionTitle ? <li>Īsumā: {item.descriptionTitle}</li> : ""}
-                        {item.link ? <li>{item.link}</li> : ""}
-                        {item.descriptionHeader ? <li>{item.descriptionHeader}</li> : ""}
-                        {item.extraLink1 ? <li>{item.extraLink1}</li> : ""}
-                        {item.extraLink2 ? <li>{item.extraLink2}</li> : ""}
-                        {item.extraLink3 ? <li>{item.extraLink3}</li> : ""}
-
-                      </ul>
-                    </div>
-                  </div>
-                </InfoWindow>
-                }
-
-              </Marker> */}
-
-
-              {/* this enables lines of atecedance */}
-              {/* < Polyline
-                path={
-                  [
-                    {
-                      lat: originLat,
-                      lng: originLng
-                    },
-                    {
-                      lat: selfLat,
-                      lng: selfLng
-                    }
-                  ]
-                }
-                options={{
-                  geodesic: true,
-                  strokeColor: '#ffa18c',
-                  strokeOpacity: item.isKnown == 1 ? 0.3 : 0,
-                  strokeWeight: 2.2,
-                  icons: [{
-                    icon: state.lineSymbol,
-                    scale: 200,
-                    repeat: '20px'
-
-                  }],
-                }}
-              /> */}
-
-              {/* IF YOU CAN FIGURE OUT HOW TO PUT TEXT INSIDE A CIRCLE - THIS WILL LOOK BETTER */}
-              {/* <Circle
-                center={{
-                  lat: 57.235968,
-                  lng: 23.779556
-                }}
-                radius={20000}
-                options={{
-                  geodesic: true,
-                  fillColor: "#5a617e",
-                  strokeColor: '#f50530',
-                  strokeWeight: 0.2,
-
-                }}
-                lable={"asdf"}
-                text={"asdf"}
-                zIndex={1000000}
-                onCloseClick={props.onToggleOpen}
-              >
-              </Circle> */}
 
             </div>
           )
@@ -393,12 +172,7 @@ const MapWithoutPolylines = compose(
         })
       }
 
-      {/* STATIC MARKER WITH RIGA COORDINATES */}
-      {/* <Marker
-      position={{
-        lat: 56.955332,
-        lng: 24.090854
-      }} /> */}
+
 
       {
         <InfoWindow
@@ -410,7 +184,6 @@ const MapWithoutPolylines = compose(
         >
           <div style={{ opacity: 0.75 }}>
             <h2>LV Kopā: {props.infectedPeople.length}</h2>
-            {/* <p>{(new Date()).getUTCDate() + "." + (new Date()).getUTCMonth() + "." + (new Date()).getFullYear()}</p> */}
             <p>{state.timeNow}</p>
           </div>
         </InfoWindow>
--- ui/src/Home.jsx
+++ ui/src/Home.jsx
@@ -22,8 +22,6 @@ moment.locale("lv")
 
 
 
-let a = [{ "id": "1", "title": "Vakcīna Vēl NAV Izstrādāta, Vakcīnas Jaunumi:", "content": "Pasaules Veselības Oranizācijas Apvienotā Pētniecības Datubāze un vakcīnas kandidātu apkopojums:", "linkTitle1": "WHO Global Research Database", "link1": "https://www.who.int/blueprint/priority-diseases/key-action/novel-coronavirus/en/", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "2", "title": "Mirstība ir 5 no 100", "content": "45 526 Nāves Gadījumi Pasaulē 02.04, 4924 jauni nāves gadījumi no 01.04-02.04, 896 450 Inficētie Pasaulē 02.04, 72 839 gadījumi no 01.04-02.04. Ņemiet vērā ka dati ir aptuveni, jo autokrātiski režīmi kā Ķīna vai Krievija var būt sniegusi ļoti neprecīzus datus WHO.", "linkTitle1": "WHO 73. Situācijas Atskaite 21.20", "link1": "https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200329-sitrep-69-covid-19.pdf?sfvrsn=8d6620fa_2", "linkTitle2": "Autokrātiski Režīmi Iespējams Sniedz Nepatiesus Datus Par COVID-19 Izplatību", "link2": "https://www.youtube.com/watch?v=RFftsn6izic", "linkTitle3": "", "link3": "" }, { "id": "3", "title": "Kādi Ir Vīrusa Simptomi:", "content": "- Temperatūra (87.9%),\n- Sauss Klepus (67.7%)\n- Nogurums (38.1%)\n- Krēpas kaklā (33.4%)\n- Elpas Trūkums (18.6%)\n- Sakairināts kakls(13.9%)\n- Galvassāpes (13.6%)\n- Muskuļu Sāpes (14.8%)\n- Drebuļi (11.4%)\n- Slikta dūša un vemšana (5.0%)\n- Ciet Degums (4.8%)\n- Caureja (3.7%)\n- hemoptysis (0.9%),\n- and conjunctival congestion (0.8%)", "linkTitle1": "WHO FAQ", "link1": "https://www.who.int/news-room/q-a-detail/q-a-coronaviruses", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "4", "title": "Kādi Ir Dezinfekcijas Šķidrumi:", "content": ">70% etanols + 0.5% ūdeņraža peroksīds VAI 0.1 % nātrija hipohlorīds. (hloroheksedīns nav tik efektīvs). Jātur uz virsmas vismaz minūte. Ņemiet vērā, hlors un peroksīds koncentrētā formā ir ļoti kodīgs šķidrums. Rūpīgi turiet burciņu. Nepārspīlējiet ar koncentrāciju.", "linkTitle1": "NCBI Report 06.02", "link1": "https://www.ncbi.nlm.nih.gov/pubmed/32035997", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "5", "title": "Cik Ilgi Koronavīrss Izdzīvo Uz Nedzīvām Virsmām:", "content": "22 pētījumu analīzē konstatēts, ka cilvēka koronavīrusi tādi kā \"Severe Acute Respiratory Syndrome\" (SARS) un \"Middle East Respiratory Syndrome\" (MERS) vai endēmiskie koronavīrusi  (HCoV) var saglabāties uz nedzīvām virsmām, piemēram, metāla, stikla vai plastmasas, līdz - 9 dienām.", "linkTitle1": "NCBI Report 06.02", "link1": "https://www.ncbi.nlm.nih.gov/pubmed/32035997", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "6", "title": "Par Maskām:", "content": "Lūdzu ņemiet vērā, ka corona vīruss ir 60-140 nanometru liels. Ja nav pieejamas maskas, jebkāda aizsardzība ir labāka par nekādu aizsardzību, bet labākā aizsardzība nāktu no neausta auduma, ar pēc iespējas mazāku poru lielumu. Tas iekļauj parastās sejas maskas vai būvniecības respirātorus. Būvniecības respirātoriem būtu jādod priekšroka, jo tie labāk pieguļ sejai. Kokvilnai vai cita veida austajiem audumiem starp šķiedrām ir lielas poaras, kurām vīruss ir spējīgs izlidot cauri. Ņemier vērā ka virusa infekcijas galvenais ceļš tomēr ir fizisks kontakts, nevis gaiss.", "linkTitle1": "National Geographic 27.03.20, Coronavirus 101: What you need to know", "link1": "https://www.nationalgeographic.com/science/2020/03/covid-overview-coronavirus/", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "7", "title": "Par Informāciju:", "content": "Lūdzu esiet Ļoti skeptiski par medicīniska vai epidemioloģiskā rakstura informāciju no sociālajiem mēdijiem vai presi. Tas iekļauj ziņu portālus un sociālos mēdijus. Uzticiet savu un citu cilvēku dzīvību uzticamiem resursiem kā Slimību Profilakses un Kontroles Centra izsludinātajai informācijai un Pasaules Veselības Organizācijas izsludinātajai informācijai, kā arī pētijumiem, kurus ir publicējuši uzticami resursi. Kā arī ņemiet vērā, ka statistika un informācija, kura nāk no autokrātiskiem režīmiem kā Ķīna vai Krievija, vai salīdzinājumi ar šīm valstīm in neprecīzi un nepatiesi.", "linkTitle1": "Autokrātiski Režīmi Iespējams Sniedz Nepatiesus Datus Par COVID-19 Izplatību", "link1": "https://www.youtube.com/watch?v=RFftsn6izic", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "8", "title": "Kas ir Korona Vīruss un COVID-19:", "content": "Vīrusi ir mazas čaulas ar ģenētisko kodu. 1000 reižu mazāki par mikrobu. Viņi tiek cilvēku šūnu kodolos savieno savu ģenētisko kodu ar mūsu ģenētisko kodu un liek mūsu pašu šūnām viņus pavairot. Tas ir viss ko viņi dara. Covid-19 ir slimība kuru raksturo viņas simptomi un kuru izraisa nesen atklāta korona vīrusa šķirne. Korona vīrusi var inficēt gan cilvēkus gan dzīvniekus. Korona vīrusi savā starpā ir ģenētiski līdzīgi. Korona vīrusi pārsvarā izraisa elpceļu saslimšanu. Pie koronavīrusiem pieder \"Middle East Respiratory Syndrome\" (MERS) un \"Severe Acute Respiratory Syndrome\" (SARS). Patreizējo pandēmiju ir izraisījis vīrusa paveids.", "linkTitle1": "WHO Report 24.02", "link1": "https://www.who.int/docs/default-source/coronaviruse/who-china-joint-mission-on-covid-19-final-report.pdf", "linkTitle2": "Features, Evaluation and Treatment Coronavirus (COVID-19), 20.03.20", "link2": "https://www.ncbi.nlm.nih.gov/books/NBK554776/", "linkTitle3": "", "link3": "" }, { "id": "9", "title": "Kādas Pasaulei Varbūt Sekas Ja Tu Nepaliksi Mājās:", "content": "Miljoniem Cilvēku Mirs. Pat, ja tev būs nebūs COVID-19 smagā formā. Kā slimības pārnēsātājs tu vari inficēt citu cilvēku, kurš var nomirt. ", "linkTitle1": "", "link1": "", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "10", "title": "Kāpēc Ir Svarīgi Palikt Mājās:", "content": "Jo vīruss eksistē vienīgi iekš cilvēka un 9 dienas uz virsmām. Ja visa pasaule maģiski paliktu mājās 3-4 nedēļas. Vīrusa izplatību būtu iespejams ierobežot līdz minimumam", "linkTitle1": "NCBI Report 06.02", "link1": "https://www.ncbi.nlm.nih.gov/pubmed/32035997", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "11", "title": "Kādas Ir Komplikācijas Ja Izārstējas:", "content": "Tiek pētīts.", "linkTitle1": "", "link1": "", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "12", "title": "Kad Vīrusu Atklāja:", "content": "2019. gada Decembrī, Ķīnā.", "linkTitle1": "WHO Report 24.02", "link1": "https://www.who.int/docs/default-source/coronaviruse/who-china-joint-mission-on-covid-19-final-report.pdf", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }, { "id": "13", "title": "Kas Draud, ja Vīruss Mutēsis:", "content": "Viņš var kļūt nāvējošāks un izstrādāt rezistenci pret pret iespējamām vakcīnām. ", "linkTitle1": "", "link1": "", "linkTitle2": "", "link2": "", "linkTitle3": "", "link3": "" }]
-
 class Home extends Component {
 
 	userSettings = localStorage.getItem("latvijaskoronakartesSettings") ? JSON.parse(localStorage.getItem("latvijaskoronakartesSettings")) : {}
@@ -86,6 +84,7 @@ class Home extends Component {
 
 	}
 
+
 	componentDidMount() {
 
 		this.fetchData("/tvnet", this.state.tvnetRssHash, "tvnetRss", "tvnetRssHash", "latvijaskoronakartesTvnetRss", "latvijaskoronakartesTvnetRssHash")
@@ -93,6 +92,7 @@ class Home extends Component {
 		this.fetchData("/infected", this.state.infectedPeopleHash, "infectedPeople", "infectedPeopleHash", "latvijaskoronakartesInfectedPeople", "latvijaskoronakartesInfectedPeopleHash")
 
 	}
+
 
 	fetchData = (slug, hash, thisStateProperyName, thisStatePropertyHashName, localStorageItemName, localStorageItemHashName) => {
 
@@ -117,7 +117,7 @@ class Home extends Component {
 
 						localStorage.setItem(localStorageItemName, JSON.stringify(result.data))
 						localStorage.setItem(localStorageItemHashName, result.hash)
-						this.setState({ [thisStateProperyName]: result.data })
+						this.setState({ [thisStateProperyName]: result.data }) // This is the cause. If I turn off the state update, the browser crash never happens. It somehow makes everything rerender or something. 
 
 					}
 
@@ -249,6 +249,9 @@ class Home extends Component {
 			<Segment inverted={true}>
 
 
+				{/* ####################### */}
+				{/* THIS IS AN ERROR WINDOW */}
+				{/* ####################### */}
 				{hasError && <Modal open={this.state.hasError} basic size="small">
 					<Header icon="american sign language interpreting" content="Atvainojos Par Traucējumu" />
 					<Modal.Content>
@@ -264,11 +267,26 @@ class Home extends Component {
 				</Modal>}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 				<Grid divided stackable >
 					<Grid.Row>
 
+						{/* ########################## */}
+						{/* THIS IS THE TWITTER WINDOW */}
+						{/* ########################## */}
 						<Grid.Column stackable="true" width={3} divided="true"  >
-
 
 
 							<Header className="box-header" inverted={true} as="h4">SPKC Twittera Tvīti</Header>
@@ -286,6 +304,17 @@ class Home extends Component {
 
 
 
+
+
+
+
+
+
+
+
+							{/* ############################ */}
+							{/* THIS IS THE TVNET RSS WINDOW */}
+							{/* ############################ */}
 							<Header className="box-header" inverted={true} as="h4">TvNet Korona Ziņas</Header>
 
 							<div style={{ overflow: "auto", maxHeight: 35 + "vh" }}>
@@ -314,6 +343,25 @@ class Home extends Component {
 
 						</Grid.Column>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+						{/* ###################### */}
+						{/* THIS IS THE MAP WINDOW */}
+						{/* ###################### */}
 						<Grid.Column stackable="true" width={10} className={"map-container"} >
 
 
@@ -343,11 +391,19 @@ class Home extends Component {
 
 
 
+
+
+
+
+
+
+
 						<Grid.Column width={3} divided="true" >
 
-
+							{/* ############################## */}
+							{/* THIS IS THE KNOWN CASES WINDOW */}
+							{/* ############################## */}
 							<Header className="box-header" as="h4" inverted={true}>Atklātie Gadījumi</Header>
-
 
 
 							<div style={{ overflow: "auto", maxHeight: 20 + "vh" }}>
@@ -393,10 +449,26 @@ class Home extends Component {
 
 
 
+
+
+
+
+
+
+
+
+
+							{/* ############################# */}
+							{/* THIS IS THE FACTS/INFO WINDOW */}
+							{/* ############################# */}
 							<Header className="box-header" inverted={true} as="h4">Korona/COVID-19 Fakti</Header>
 							<div style={{ overflow: "auto", maxHeight: 40 + "vh" }}>
 
+
+
+								{/* THESE TWO ARE HARD CODED */}
 								<Accordion key={0} inverted={true} styled>
+
 
 									<Accordion.Title className={"accordion-title"} inverted="true" active={activeFirstFactIndex === -1 ? false : true} index={0} onClick={this.handleFirstFactClick}>
 										<Icon corner name="dropdown" />
@@ -429,59 +501,79 @@ class Home extends Component {
 									</Accordion.Content>
 
 
-
-
-
-
 								</Accordion>
 
 
-							{facts.map((el, i) => {
-								return <Accordion key={Math.random() * i + 2} inverted={true} styled>
 
-									<Accordion.Title className={"accordion-title"} inverted="true" active={activeFactIndex === i} index={i} onClick={this.handleFactClick}>
-										<Icon corner name="dropdown" />
-										{el.title}
-									</Accordion.Title>
 
-									<Accordion.Content style={{ color: "white", background: "#525252" }} className={"accordion-content"} active={activeFactIndex === i}>
-										{el.content}
-										<br></br>
-										{el.link1 && <a href={el.link1 || "http://google.com"}>{el.linkTitle1 || "resurss"}</a>}
-										<br></br>
-										{el.link2 && <a href={el.link2 || "http://google.com"}>{el.linkTitle2 || "resurss"}</a>}
-										<br></br>
-										{el.link3 && <a href={el.link3 || "http://google.com"}>{el.linkTitle3 || "resurss"}</a>}
-									</Accordion.Content>
+								{/* THESE ARE THE FACTS GOTTEN FROM THE API */}
+								{facts.map((el, i) => {
+									return <Accordion key={Math.random() * i + 2} inverted={true} styled>
 
-								</Accordion>
-							})}
+										<Accordion.Title className={"accordion-title"} inverted="true" active={activeFactIndex === i} index={i} onClick={this.handleFactClick}>
+											<Icon corner name="dropdown" />
+											{el.title}
+										</Accordion.Title>
+
+										<Accordion.Content style={{ color: "white", background: "#525252" }} className={"accordion-content"} active={activeFactIndex === i}>
+											{el.content}
+											<br></br>
+											{el.link1 && <a href={el.link1 || "http://google.com"}>{el.linkTitle1 || "resurss"}</a>}
+											<br></br>
+											{el.link2 && <a href={el.link2 || "http://google.com"}>{el.linkTitle2 || "resurss"}</a>}
+											<br></br>
+											{el.link3 && <a href={el.link3 || "http://google.com"}>{el.linkTitle3 || "resurss"}</a>}
+										</Accordion.Content>
+
+									</Accordion>
+								})}
+
+
+
+
+
 							</div>
 
 
 
-						<Header className="box-header" inverted={true} as="h4">Par Karti</Header>
-						<div style={{ overflow: "auto", maxHeight: 20 + "vh" }}>
-							<Accordion key={0} inverted={true} styled>
 
-								<Accordion.Title className={"accordion-title"} inverted="true" active={activeMapAccordionIndex === -1 ? false : true} index={0} onClick={this.handleMapAccordionClick}>
-									<Icon corner name="dropdown" />
+
+
+
+
+
+
+
+
+
+
+
+
+							{/* ############################ */}
+							{/* THIS IS THE ABOUT MAP WINDOW */}
+							{/* ############################ */}
+							<Header className="box-header" inverted={true} as="h4">Par Karti</Header>
+							<div style={{ overflow: "auto", maxHeight: 20 + "vh" }}>
+								<Accordion key={0} inverted={true} styled>
+
+									<Accordion.Title className={"accordion-title"} inverted="true" active={activeMapAccordionIndex === -1 ? false : true} index={0} onClick={this.handleMapAccordionClick}>
+										<Icon corner name="dropdown" />
 									Kā interpretēt kartē attēloto
 									</Accordion.Title>
 
-								<Accordion.Content style={{ color: "white", background: "#525252", paddingLeft: "1.5em" }} className={"accordion-content"} active={activeMapAccordionIndex === -1 ? false : true}>
-									<li>Ņemiet vērā, ka vairums uz kartes esošajiem punktiem ir aptuvenas informācijas vizualizācija un nenorāda konkrētas adreses. To var noskaidrot uzklikšķinot uz interesējošā punkta</li>
-									<li>Visi inficētie kuriem nav zināma atrašanās vieta atrodas Rīgā</li>
-									<li>Punkti tiek katru dienu atjaunoti, lai attēlotu pēdējo SPKC attēlojumu pa reģioniem</li>
-									<li>Ja nav publicēta konkrētāka apstiprinātās personas atrašanās vieta, tad inficētais gadījums tiks novietots SPKC publicētās kartes reģiona lielākajā apdzīvotajā teritorijā</li>
-									<li>Piemēram: ja Rīgā ir norādīts, ka konkrētā dienā ir no 51-100 inficētajiem un Jelgavas reģionā 1-5, tad Rīgā tiek ielikts 51 punkts un Jelgavā tiks ielikts 1 punkts</li>
-									<li>Sarkanā krāsā ir iezīmētas apdzīvotas vietas un ceļi</li>
-									<li>Ja ir vēl jautājumi par to, kā interpretēt karti, jautājiet sūtot jautājumu uz ēpastu</li>
+									<Accordion.Content style={{ color: "white", background: "#525252", paddingLeft: "1.5em" }} className={"accordion-content"} active={activeMapAccordionIndex === -1 ? false : true}>
+										<li>Ņemiet vērā, ka vairums uz kartes esošajiem punktiem ir aptuvenas informācijas vizualizācija un nenorāda konkrētas adreses. To var noskaidrot uzklikšķinot uz interesējošā punkta</li>
+										<li>Visi inficētie kuriem nav zināma atrašanās vieta atrodas Rīgā</li>
+										<li>Punkti tiek katru dienu atjaunoti, lai attēlotu pēdējo SPKC attēlojumu pa reģioniem</li>
+										<li>Ja nav publicēta konkrētāka apstiprinātās personas atrašanās vieta, tad inficētais gadījums tiks novietots SPKC publicētās kartes reģiona lielākajā apdzīvotajā teritorijā</li>
+										<li>Piemēram: ja Rīgā ir norādīts, ka konkrētā dienā ir no 51-100 inficētajiem un Jelgavas reģionā 1-5, tad Rīgā tiek ielikts 51 punkts un Jelgavā tiks ielikts 1 punkts</li>
+										<li>Sarkanā krāsā ir iezīmētas apdzīvotas vietas un ceļi</li>
+										<li>Ja ir vēl jautājumi par to, kā interpretēt karti, jautājiet sūtot jautājumu uz ēpastu</li>
 
-								</Accordion.Content>
+									</Accordion.Content>
 
-							</Accordion>
-						</div>
+								</Accordion>
+							</div>
 
 
 						</Grid.Column>
@@ -491,7 +583,7 @@ class Home extends Component {
 
 				</Grid>
 
-			<p>Karti uztur: https://github.com/snotrman/corona-map, nils.riga@gmail.com</p>
+				<p>Karti uztur: https://github.com/snotrman/corona-map, nils.riga@gmail.com</p>
 
 			</Segment >
 		);
